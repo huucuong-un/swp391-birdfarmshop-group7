3c5ceda18f3f15562566faaf8de1cfe7f333903a
leekongvine123 <congvinhdt1223@gmail.com>
2023-10-19 14:15:48 +0700

    fix total page for feedback and order

--- backend/parrotfarmshop/src/main/java/com/eleventwell/parrotfarmshop/controller/OrderController.java
+++ backend/parrotfarmshop/src/main/java/com/eleventwell/parrotfarmshop/controller/OrderController.java
@@ -86,7 +86,7 @@ public class OrderController {
         }
 
         result.setListResult(orderResponses);
-        result.setTotalPage(((int) Math.ceil((double) (result.getListResult().size() + 1) / limit)));
+        result.setTotalPage(((int) Math.ceil((double) (orderService.totalItem()) / limit)));
         result.setLimit(limit);
         return result;
     }

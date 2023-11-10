fdf86d6ee55a56c360c6e0daa49e03f372a5a614
leekongvine123 <congvinhdt1223@gmail.com>
2023-11-11 02:07:36 +0700

    fix promotion. fix send mail

--- backend/parrotfarmshop/src/main/java/com/eleventwell/parrotfarmshop/entity/FeedbackEntity.java
+++ backend/parrotfarmshop/src/main/java/com/eleventwell/parrotfarmshop/entity/FeedbackEntity.java
@@ -73,7 +73,7 @@ public class FeedbackEntity extends BaseEntity {
 	private ParrotSpeciesColorEntity parrotSpeciesColor;
 
 	@OneToOne
-	@JoinColumn(name = "order_detail_ids", unique = true,updatable = false)
+	@JoinColumn(name = "order_detail_id", unique = true,updatable = false)
 	private OrderDetailEntity orderDetailId;
 
 
